0c53f6be5364200070678a1819d3dd7e8504e5b9
hyun331 <sus03319@gmail.com>
2024-05-14 17:43:26 +0900

    0514

--- 3.shell_programming.sh
+++ 3.shell_programming.sh
@@ -59,10 +59,74 @@ done                # 끝
 
 
 # if문 실습 - test4.sh
--echo로 start 출력
--만약에 test_dir이 현재 폴더에 있으면 
--없으면 해당 폴더 생성
--echo로 end 출력
+# echo로 start 출력
+# 만약에 test_dir이 현재 폴더에 있으면 
+# 없으면 해당 폴더 생성
+# echo로 end 출력
+cd
+if [ -d test_dir ]; then
+    cd test_dir
+else
+    mkdir test_dir
+    cd test_dir
+fi
 
--test4.txt 없으면 생성
--해당 파일에 hello world1~100까지 입력하도록
+# test4.txt 없으면 생성
+# 해당 파일에 hello world1~100까지 입력하도록
+if [ -f test4.txt ]; then
+    echo "test4 file exists"
+else
+    touch test4.txt  
+fi
+for a in {1..100}
+do
+    echo "hello world$a" >> test4.txt
+done
+
+# for문 활용한 count 세기
+count=0
+for
+do
+    ((count++)) #(())를 이용하여 ++
+done
+echo "count is $count"
+
+# for문을 활용한 모든 파일, 폴더 목록 출력하기
+for a in *      #현재 폴더에 있는 파일, 목록을 하나씩 a에 둔다.
+
+# for문 count, 목록 출력 실습
+# 특정 디렉토리 안에 file은 몇개인지 directory는 몇개 인지
+dcount=0
+fcount=0
+
+for a in *
+do
+    if [ -d "$a" ]; then  
+        ((dcount++))
+    else
+        ((fcount++))
+    fi
+done
+echo "directory is $dcount"
+echo "file is $fcount"
+
+
+# apt-get이라는 패키지에서 필요한걸 다운받기
+패키지 = 프로그램 목록
+우분투에서는 apt 사용
+sudo apt update     //업데이트
+sudo apt install nginx
+
+#find를 통해 특정파일을 찾고 grep을 이용해서 문자열 찾기
+ps -ef : 모든프로세스 찾는 옵션 -> grep을 통해 원하는  프로세스 검색 |
+ps -ef | greap "nginx"  # master 어쩌구랑 여러개 나옴.
+sudo kill -9 pid        # 죽이고 localhost:80 들어가면 안보임
+sudo systemctl start nginx
+ps -ef | grep "nginx"   # localhost:80 접속하면 성공!
+
+
+#grep
+grep -rin "hi"
+
+#find
+find ~ | grep -rin "hi"
